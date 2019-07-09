c54fa660686467e57cb424dea35e46936951a4a3
wangchuan <chuan.wang@okcoin.com>
2019-07-09 11:45:28 +0800

    ci 测试3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './style.less';
 
-document.addEventListener("DOMContentLoaded", () => {
-  const a = 'test';
-  console.log(a);
-  document.body.innerHTML = a;
-}, false);
+// document.addEventListener("DOMContentLoaded", () => {
+//   const a = 'test1222';
+//   // document.body.innerHTML = a;
+//
+// }, false);
+
+$(document).ready(function () {
+  $.ajax({
+    url: '../api/issueList.json',
+    async: false,    success: function (data) {
+      console.log(data)
+    }
+  });
+});
